e5a12a21ee721f49f7602c02b76342aa96b469eb
jimmy1357 <512697043@qq.com>
2017-08-11 17:23:07 +0800

    Update process_data_cn.py

--- process_data_cn.py
+++ process_data_cn.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 08 13:32:23 2016
-
-@author: L
-"""
-
 ''''
 CNN 文本分类之中文预处理(分词)
-E:\doc\data\SogouC.reduced.20061127\SogouC.reduced\Reduced\C000024 军事
-
-E:\doc\data\SogouC.reduced.20061127\SogouC.reduced\Reduced\C000020 教育
-
 分词提取实词后，词汇量仍然相对较大，在样本和计算资源有限的情况下，可以考虑选取频率较高的特征作为原始特征
 '''
 #encoding=utf-8
@@ -18,6 +8,7 @@ import jieba
 import jieba.posseg as pseg
 import os
 
+# 排除掉某些词性的词
 flag_list = ['t','q','p','u','e','y','o','w','m']
 def jiebafenci(all_the_text):
     re = ""
@@ -45,26 +36,43 @@ def jiebafenci(all_the_text):
         relist = re
     relist = relist + "\n"
     relist = relist.replace("\r\n","\n").replace("\n\n","\n")
-
+    #print relist
     return relist
 
 def getTrainData(inpath,outfile):
-    fw = open(outfile,"a") 
+    fw = open(outfile,"w") 
     for filename in os.listdir(inpath):
-        print filename
+        if os.path.splitext(filename)[1] != ".txt":
+            continue
         file_object = open(inpath+"/"+filename)
         try:
             all_the_text = file_object.read()
+            #print all_the_text
             #all_the_text = all_the_text.decode("gb2312").encode("utf-8")
             pre_text = jiebafenci(all_the_text)
-            if len(pre_text)>30:
+            if len(pre_text)>1:
                 fw.write(pre_text.encode("utf-8"))
         except:
             pass
         finally:
             file_object.close()
     fw.close()
-    
-inpath = '../SogouC.reduced/Reduced/C000024'
-outfile = '../SogouC.reduced/Reduced/C000024_pre.txt'
+
+inpath = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/Jingdong_NB_4000/neg'
+outfile = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/Jingdong_NB_4000/neg/neg.cnn'
+getTrainData(inpath,outfile)
+inpath = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/Jingdong_NB_4000/pos'
+outfile = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/Jingdong_NB_4000/pos/pos.cnn'
+getTrainData(inpath,outfile)
+inpath = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/Book_del_4000/pos'
+outfile = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/Book_del_4000/pos/pos.cnn'
+getTrainData(inpath,outfile)
+inpath = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/Book_del_4000/neg'
+outfile = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/Book_del_4000/neg/neg.cnn'
+getTrainData(inpath,outfile)
+inpath = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/new_datas/pos'
+outfile = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/new_datas/pos/pos.cnn'
+getTrainData(inpath,outfile)
+inpath = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/new_datas/neg'
+outfile = '/home/workspace/classifier/Sentiment_analysis/SentimentAnalysis/new_datas/neg/neg.cnn'
 getTrainData(inpath,outfile)
